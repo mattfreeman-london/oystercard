472e2bc6472caa55ecf8042e98e021be661d79cb
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2019-10-21 16:18:42 +0100

    add touch in/touch out support

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,6 +6,7 @@ class Oystercard
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(credit)
@@ -17,6 +18,18 @@ class Oystercard
 
   def deduct(debit)
     @balance -= debit
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,22 +3,41 @@ require 'oystercard'
 describe Oystercard do
   let(:card) {Oystercard.new}
 
-  it "has a balance" do
-    expect(card.balance).to eq 0
+  context "balance on card" do
+    it "has a balance" do
+      expect(card.balance).to eq 0
+    end
+
+    it "allows you to add money to the card" do
+      card.top_up(1)
+      expect(card.balance).to eq(1)
+    end
+
+    it "doesn't allow you to add more money than the limit to the card" do
+      expect{ card.top_up(100) }.to raise_error("Maximum balance is £#{Oystercard::BALANCE_LIMIT}.")
+    end
+
+    it "allows you to deduct money from the card" do
+      card.deduct(5)
+      expect(card.balance).to eq(-5)
+    end
+
   end
 
-  it "allows you to add money to the card" do
-    card.top_up(1)
-    expect(card.balance).to eq(1)
-  end
+  context "is the card in use?" do
+    it "the card is in use" do
+      expect(card.in_journey?).to eq false
+    end
 
-  it "doesn't allow you to add more money than the limit to the card" do
-    expect{ card.top_up(100) }.to raise_error("Maximum balance is £#{Oystercard::BALANCE_LIMIT}.")
-  end
+    it "allows you to touch in to start a journey" do
+      card.touch_in
+      expect(card.in_journey?).to eq true
+    end
 
-  it "allows you to deduct money from the card" do
-    card.deduct(5)
-    expect(card.balance).to eq(-5)
+    it "allows you to touch out to end a journey" do
+      card.touch_out
+      expect(card.in_journey?).to eq false
+    end
   end
 
 end
